79befd6f0600689fdb808087d9a6c7b42bb8d0bc
Igor Mironchik <igor.mironchik@gmail.com>
2026-01-24 07:48:39 +0300

    Correct a script.

--- script/windows/clean.bat
+++ script/windows/clean.bat
@@ -5,5 +5,4 @@ del /Q aqtinstall.log
 rmdir /S /Q installer\packages\mironchik.igor.gif\data\bin
 rmdir /S /Q installer\packages\mironchik.igor.gif\data\lib
 rmdir /S /Q installer\packages\mironchik.igor.gif\data\plugins
-rmdir /S/ Q installer\packages\mironchik.igor.gif\data\resources
 rmdir /S /Q installer\packages\mironchik.igor.gif\data\translations
