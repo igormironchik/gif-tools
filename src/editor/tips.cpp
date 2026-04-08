b8d0f73cfbaf02ba2de687ee500ed9ee2542bdc2
Igor Mironchik <igor.mironchik@gmail.com>
2026-04-08 10:50:14 +0300

    Format the code.

--- src/editor/tips.cpp
+++ src/editor/tips.cpp
@@ -4,8 +4,8 @@
 */
 
 // GIF editor include.
-#include "constants.hpp"
 #include "tips.hpp"
+#include "constants.hpp"
 
 // Qt include.
 #include <QGridLayout>
@@ -28,38 +28,41 @@ Tips::Tips(QWidget *parent)
     t->setContentsMargins(10, 10, 10, 10);
     t->setAlignment(Qt::AlignTop | Qt::AlignLeft);
     t->setWordWrap(true);
-    t->setText(
-        tr("<h1>Tips & Tricks</h1>"
-           "<ul><li>"
-           "GIF editor can play GIFs. There is a button for this on the tool bar. You can use "
-           "Space key for start/stop playing."
-           "</li>"
-           "<li>"
-           "On the tape you can use context menu for such actions like - saving the frame into disk, "
-           "setting delay after the frame..."
-           "</li>"
-           "<li>"
-           "You can use different edit tools, like"
-           "<ul>"
-           "<li>"
-           "<p>Crop entire GIF. When crop is started...</p>"
-           "<p>%1</p>"
-           "</li>"
-           "<li>"
-           "<p>Add text into GIF. When tool is started...</p>"
-           "<p>%2</p>"
-           "</li>"
-           "<li>"
-           "<p>Add a rectangle into GIF. When tool is started...</p>"
-           "<p>%3</p>"
-           "</li>"
-           "<li>"
-           "<p>Add an arrow into GIF. When tool is started...</p>"
-           "<p>%4</p>"
-           "</li>"
-           "</ul>"
-           "</li>"
-           "</ul>").arg(s_cropHelp).arg(s_textHelp).arg(s_rectHelp).arg(s_arrowHelp));
+    t->setText(tr("<h1>Tips & Tricks</h1>"
+                  "<ul><li>"
+                  "GIF editor can play GIFs. There is a button for this on the tool bar. You can use "
+                  "Space key for start/stop playing."
+                  "</li>"
+                  "<li>"
+                  "On the tape you can use context menu for such actions like - saving the frame into disk, "
+                  "setting delay after the frame..."
+                  "</li>"
+                  "<li>"
+                  "You can use different edit tools, like"
+                  "<ul>"
+                  "<li>"
+                  "<p>Crop entire GIF. When crop is started...</p>"
+                  "<p>%1</p>"
+                  "</li>"
+                  "<li>"
+                  "<p>Add text into GIF. When tool is started...</p>"
+                  "<p>%2</p>"
+                  "</li>"
+                  "<li>"
+                  "<p>Add a rectangle into GIF. When tool is started...</p>"
+                  "<p>%3</p>"
+                  "</li>"
+                  "<li>"
+                  "<p>Add an arrow into GIF. When tool is started...</p>"
+                  "<p>%4</p>"
+                  "</li>"
+                  "</ul>"
+                  "</li>"
+                  "</ul>")
+                   .arg(s_cropHelp)
+                   .arg(s_textHelp)
+                   .arg(s_rectHelp)
+                   .arg(s_arrowHelp));
 
     setWidget(t);
 }
