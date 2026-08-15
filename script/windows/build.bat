bdd3ae94bb6720b91020c6778073fe688804b7ae
Igor Mironchik <igor.mironchik@gmail.com>
2026-08-15 10:30:09 +0300

    Fix build script

--- script/windows/build.bat
+++ script/windows/build.bat
@@ -7,8 +7,6 @@ set "cwd=%current_dir:\=/%"
 
 echo "Building gif-tools..."
 
-mkdir build-gif-tools
-
 cmake -DCMAKE_BUILD_TYPE=Release -S . -B "%cwd%/../builds/build-gif-tools" -DCMAKE_PREFIX_PATH="%cwd%/../Qt/%qt_version%/%qt_arch%;%cwd%/../KDE" -G "NMake Makefiles"
 
 IF %ERRORLEVEL% NEQ 0 (
